453544968ce63d006d80acc72c7dcd898baf44be
Luna Hatahet <luna.hatahet@gmail.com>
2023-05-17 22:13:08 +0400

    Added pagination links

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -6,15 +6,39 @@ import AvailableLists from "../components/Todos/AvailableLists";
 import Pagination from "../components/Todos/Pagination";
 
 function HomePage(props) {
-  const [pageSize, setPageSize] = useState(5); 
+  const [pageSize, setPageSize] = useState(5);
   const [lists, setLists] = useState(props.lists);
+  const [page, setPage] = useState(1);
+  const [pageCount, setPageCount] = useState(props.pageCount);
 
   const handlePageSizeChange = async (event) => {
     const newSize = parseInt(event.target.value);
     setPageSize(newSize);
+    setPage(1);
     try {
       const response = await axios.get(
         `http://localhost:8000/?pageSize=${newSize}`,
+        {
+          headers: {
+            Authorization:
+              "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJjZTY1NGFlNi01M2FhLTQwNDQtODc5Mi1kMTBjNjMyNDU1ZjQiLCJpYXQiOjE2ODQxODQ3MDZ9.NUmQBiCTChFXFcJaUMPH1_8fVSPwtFtXyJI7xPRXVpE",
+          },
+        }
+      );
+      setLists(response.data.lists);
+      setPageCount(response.data.pageCount);
+    } catch (error) {
+      console.log(error);
+      setLists([]);
+      setPageCount(0);
+    }
+  };
+
+  const handlePageChange = async (newPage) => {
+    setPage(newPage);
+    try {
+      const response = await axios.get(
+        `http://localhost:8000/?pageSize=${pageSize}&page=${newPage}`,
         {
           headers: {
             Authorization:
@@ -31,7 +55,13 @@ function HomePage(props) {
 
   return (
     <Fragment>
-      <Pagination pageSize={pageSize} onPageSizeChange={handlePageSizeChange} />
+      <Pagination
+        pageSize={pageSize}
+        pageCount={pageCount}
+        currentPage={page}
+        onPageSizeChange={handlePageSizeChange}
+        onPageChange={handlePageChange}
+      />
       <AvailableLists lists={lists} />
     </Fragment>
   );
@@ -46,10 +76,12 @@ export async function getStaticProps() {
       },
     });
     const lists = response.data.lists;
+    const pageCount = response.data.pageCount;
 
     return {
       props: {
         lists,
+        pageCount,
       },
       revalidate: 1,
     };
@@ -58,6 +90,7 @@ export async function getStaticProps() {
     return {
       props: {
         lists: [],
+        pageCount: 0,
       },
       revalidate: 1,
     };
